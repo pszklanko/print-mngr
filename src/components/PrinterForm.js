fb0674e3249fb99888fc160389ad48fe8db397c1
Piotr Szklanko <pszklanko@gmail.com>
2018-03-05 23:36:52 +0100

    refactoring

--- src/components/PrinterForm.js
+++ src/components/PrinterForm.js
@@ -17,53 +17,53 @@ export class PrinterForm extends Component {
             < Form onSubmit={submittedValues => this._onSubmit(submittedValues)} defaultValues={this.props.data}>
                 {
                     formApi => (
-                        <form onSubmit={formApi.submitForm} id="form2">
+                        <form onSubmit={formApi.submitForm} id="form1">
                             <div className={'form-group ' + (formApi.errors && formApi.touched.name && formApi.errors.name ? 'has-error' : '')}>
-                                <label htmlFor="name" className="mb-4 control-label">Name</label>
+                                <label htmlFor="name" className="control-label">Name</label>
                                 <Text field="name" id="name" className="form-control" validate={validateName} />
                                 <span className="help-block">
                                     {formApi.errors && formApi.touched.name && formApi.errors.name ? formApi.errors.name : ''}
                                 </span>
                             </div>
                             <div className={'form-group ' + (formApi.errors && formApi.touched.ipAddress && formApi.errors.ipAddress ? 'has-error' : '')}>
-                                <label htmlFor="ipAddress" className="mb-4 control-label">IP address</label>
+                                <label htmlFor="ipAddress" className="control-label">IP address</label>
                                 <Text field="ipAddress" id="ipAddress" className="form-control" validate={validateIpAddress} />
                                 <span className="help-block">
                                     {formApi.errors && formApi.touched.ipAddress && formApi.errors.ipAddress ? formApi.errors.ipAddress : ''}
                                 </span>
                             </div>
                             <div className={'form-group'}>
-                                <label htmlFor="status" className="mb-4 control-label">Status</label>
+                                <label htmlFor="status" className="control-label">Status</label>
                                 <Text field="status" id="status" className="form-control" disabled />
                             </div>
-                            <label htmlFor="permissions" className="mb-4 control-label">Permissions</label>
+                            <label htmlFor="permissions" className="control-label">Permissions</label>
                             <RadioGroup field="permissions">
                                 {this.availablePermissions.map((permission, index) => (
                                     <span key={index}>
-                                        <Radio value={permission} id={permission} className="d-inline-block" />
+                                        <Radio value={permission} id={permission} />
                                         <label htmlFor={permission} className="mr-2">{permission[0].toUpperCase() + permission.slice(1)}</label>
                                     </span>
                                 ))
                                 }
                             </RadioGroup>
                             <div className={'form-group'}>
-                                <label htmlFor="description" className="mb-4 control-label">Description</label>
+                                <label htmlFor="description" className="control-label">Description</label>
                                 <TextArea field="description" id="description" className="form-control" />
                             </div>
                             <div className={'form-check'}>
                                 <Checkbox field="color" id="color" className="form-check-input" />
-                                <label htmlFor="color" className="mb-4 form-check-label">Color</label>
+                                <label htmlFor="color" className="form-check-label">Color</label>
                             </div>
                             {
                                 this.props.data.status ? (
-                                    <button type="button" onClick={() => this._onRemove()} className="mb-4 btn btn-danger">
+                                    <button type="button" onClick={() => this._onRemove()} className="btn btn-danger">
                                         Remove
                                     </button>
                                 ) : (
                                         ''
                                     )
                             }
-                            <button disabled={formApi.errors} type="submit" className="mb-4 pull-right btn btn-primary">
+                            <button disabled={formApi.errors} type="submit" className="pull-right btn btn-primary">
                                 Submit
                             </button>
                             <div className={'clearfix'}></div>
